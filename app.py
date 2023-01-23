098d0a724ef38a8eb0fcd070699adffb9f5cc086
PoalChaynik <alpolaop13@gmail.com>
2023-01-23 12:00:37 +0200

    2023

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"]=False
+CORS(app)
+
+@app.route('/')
+def index():
+    return
+
+@app.route('/Par')
+def index():
+    return 'nothing is here'
+    
+app.run(host='0.0.0.0',port=81)
